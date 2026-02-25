69081e19d3c6a46649d23edc16bbd3431611c140
pfaffelh <p.p@stochastik.uni-freiburg.de>
2026-02-25 11:23:16 +0100

    added local version deploy

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ SERVER="www2"
 USERNAME="flask-reader"
 
 # Führe den Befehl "deploy" auf dem Remote-Server aus
-ssh $USERNAME@$SERVER 'cd mi-ldap; git pull; source venv/bin/activate; python3 main.py; deactivate'
+ssh $USERNAME@$SERVER 'cd mi-ldap; git pull; source venv/bin/activate; python3 main.py --quiet; deactivate'
 
